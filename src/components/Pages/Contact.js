1965d311c3cc58140fb96cdab8f6c4d4be7fe615
Stephan Mazur <stephan.mazur5@gmail.com>
2020-11-30 10:11:51 +0100

    install formik et formulaire

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Field from '../common/Field';
+import {withFormik} from 'formik';
 
 const fields = {
     sections: [
@@ -16,20 +17,8 @@ const fields = {
 
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            name: '',
-            email: '',
-            phone: '',
-            message: ''
-        }
-    }
 
-    submitForm = (e) => {
-        alert("Form submitted")
-    }
      
     render() {
         return(
@@ -39,18 +28,22 @@ class Contact extends Component {
                         <h2 className="section-heading text-uppercase">Contact Us</h2>
                         <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     </div>
-                    <form id="contactForm" name="sentMessage" novalidate="novalidate" onSubmit={e => this.submitForm(e)}>
+                    <form id="contactForm" name="sentMessage" novalidate="novalidate" onSubmit={this.props.handleSubmit}>
                         <div className="row align-items-stretch mb-5">
                             {fields.sections.map((section, sectionIndex) =>  {
                                 return (
                                     <div className="col-md-6" key={sectionIndex} >
                                         {section.map((field, i) => {
                                             return <Field 
-                                                        {...field} 
-                                                        key={i} 
-                                                        value={this.state[field.name]}
-                                                        onChange={e => this.setState({[field.name]: e.target.value })}
-                                                    />
+                                                {...field} 
+                                                key={i}
+                                                value={this.props.values[field.name]}
+                                                name={field.name}
+                                                onChange={this.props.handleChange}
+                                                onBlur={this.props.handleBlur}
+                                                touched={(this.props.touched[field.name])}
+                                                errors={this.props.errors[field.name]}
+                                            />
                                         })}
                                     </div>
                                 )
@@ -70,4 +63,27 @@ class Contact extends Component {
     }
 }
 
-export default Contact;
+export default withFormik({
+    mapPropsToValues: () => ({
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+    }),
+    validate: values => {
+        const errors = {};
+        Object.keys(values).map( v => {
+            if(!values[v]) {
+                errors[v] = "Required";
+            }
+        })
+
+        return errors;
+
+    },
+
+    handleSubmit: (values, {setSubmitting}) => {
+        alert("You've submitted the form", JSON.stringify(values)); 
+    }
+
+})(Contact);
--- src/components/common/Field.js
+++ src/components/common/Field.js
@@ -6,27 +6,34 @@ class Field extends Component {
             <div className="form-group">
                 {this.props.elementName === 'input' ?
                 <input
-                    className="form-control"
+                    className="form-control" 
                     id={this.props.name}
                     type={this.props.type}
                     placeholder={this.props.placeholder}
-                    required="required"
-                    data-validation-required-message="Please enter your name."
-                    value={this.props.value}
-                    onChange={e => this.props.onChange(e)} 
+                    required="required" 
+                    data-validation-required-message="Please enter your name." 
+                    name={this.props.name}
+                    onChange={this.props.onChange}
+                    onBlur={this.props.onBlur}
                 />
                 :
                 <textarea 
                     className="form-control" 
                     id={this.props.name}
-                    placeholder={this.props.placeholder} 
+                    placeholder={this.props.placeholder}
                     required="required" 
                     data-validation-required-message="Please enter a message."
-                    value={this.props.value}
-                    onChange={e => this.props.onChange(e)}
+                    name={this.props.name}
+                    onChange={this.props.onChange}
+                    onBlur={this.props.onBlur}
+
                 />  
                 }
-                <p className="help-block text-danger"></p>
+                <p className="help-block text-danger">
+                    {(this.props.touched && this.props.errors) &&
+                        <span>{this.props.errors}</span>
+                    }
+                </p>
             </div>
         )
     }
